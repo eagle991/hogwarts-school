f68c58640f1aa0e4b7cc8d3a25e468788f878b00
eagle991 <eagle991@mail.ru>
2025-05-27 20:03:10 +0300

    Update GeneralController.java

--- src/main/java/ru/hogwarts/school/controller/GeneralController.java
+++ src/main/java/ru/hogwarts/school/controller/GeneralController.java
@@ -1,5 +1,6 @@
 package com.example.hogwarts.controller;
 
+import com.example.hogwarts.service.GeneralService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/general")
 public class GeneralController {
 
+    private final GeneralService generalService;
+
+    public GeneralController(GeneralService generalService) {
+        this.generalService = generalService;
+    }
+
     @GetMapping("/sum")
     public long calculateSum() {
-        return Stream.iterate(1, a -> a + 1)
-                .limit(1_000_000)
-                .parallel()
-                .reduce(0, Integer::sum);
+        return generalService.calculateSum();
     }
 }
